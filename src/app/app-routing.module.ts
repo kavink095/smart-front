c1da0184a9973f4e3811d4ab90def18141e15ab1
UKHasantha <hasanthaijse@gmail.com>
2020-11-05 14:47:30 +0530

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UsersComponent } from './users/users.component';
 import { BooksComponent } from './books/books.component';
+import { NewBookComponent } from './new-book/new-book.component';
+import { NewUserComponent } from './new-user/new-user.component';
 
 const routes: Routes = [
   { path: 'route-dashboard', component: DashboardComponent },
   { path: 'route-users', component: UsersComponent },
   { path: 'route-books', component: BooksComponent },
+  { path: 'route-new-books', component: NewBookComponent },
+  { path: 'route-new-users', component: NewUserComponent },
   { path: '', redirectTo: '/route-dashboard', pathMatch: 'full' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,24 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { UsersComponent } from './users/users.component';
 import { BooksComponent } from './books/books.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NewBookComponent } from './new-book/new-book.component';
+import { NewUserComponent } from './new-user/new-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     UsersComponent,
-    BooksComponent
+    BooksComponent,
+    NewBookComponent,
+    NewUserComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
