2915ecf263525b64575d8194a3263c6cfd50ed95
UKHasantha <hasanthaijse@gmail.com>
2020-11-11 07:29:41 +0530

    services

--- src/app/new-book/new-book.component.ts
+++ src/app/new-book/new-book.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-book',
@@ -11,5 +12,29 @@ export class NewBookComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  @ViewChild('inputwriter', { static: false }) inputElwriter: ElementRef;
+  @ViewChild('inputtag', { static: false }) inputEltag: ElementRef;
+  @ViewChild('inputdesc', { static: false }) inputEldesc: ElementRef;
+
+  formNewBook: FormGroup = new FormGroup({
+    name: new FormControl('', Validators.required),
+    writer: new FormControl('', Validators.required),
+    tagid: new FormControl('', Validators.required),
+    desc: new FormControl('', Validators.required)  
+  });
+
+  keyname(event) {
+    this.inputElwriter.nativeElement.focus();
+  }
+  keywriter(event) {
+    this.inputEltag.nativeElement.focus();
+  }
+  keytag(event) {
+    this.inputEldesc.nativeElement.focus();
+  }
+  keydesc(event) {
+    this.inputEldesc.nativeElement.focus();
+  }
+
 
 }
--- src/app/new-user/new-user.component.ts
+++ src/app/new-user/new-user.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-user',
@@ -6,10 +7,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-user.component.css']
 })
 export class NewUserComponent implements OnInit {
-
+  
   constructor() { }
+     gender: String ;
 
+
+
+  @ViewChild('inputfname', { static: false }) inputEl: ElementRef;
+  @ViewChild('inputlname', { static: false }) inputElLName: ElementRef;
+  @ViewChild('inputaddress', { static: false }) inputElAddress: ElementRef;
+  @ViewChild('inputmobile', { static: false }) inputElMobile: ElementRef;
+  @ViewChild('inputgender', { static: false }) inputElGender: ElementRef;
+  @ViewChild('inputcheck', { static: false }) inputElCheck: ElementRef;
+  
   ngOnInit(): void {
+  }
+
+  formadduser = new FormGroup({
+    id: new FormControl('', Validators.required),
+    firstname: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+    address: new FormControl('', Validators.required),
+    mobile: new FormControl('', Validators.required),
+    check: new FormControl('', Validators.required),
+    gender: new FormControl('Choose...', Validators.required)
+  });
+
+  keyID(event) {
+    console.log(event);
+    this.inputEl.nativeElement.focus();
+  }
+  keyFName(event) {
+    console.log(event);
+    this.inputElLName.nativeElement.focus();
+  }
+  keyLName(event) {
+    console.log(event);
+    this.inputElAddress.nativeElement.focus();
+  }
+  keyAddress(event) {
+    console.log(event);
+    this.inputElMobile.nativeElement.focus();
+  }
+  keyMobile(event) {
+    console.log(event);
+    this.inputElGender.nativeElement.focus();
+  }
+  keygender(event) {
+    console.log(event);
+    this.inputElCheck.nativeElement.focus();
+  }
+
+  getGenderStatus(){
+    this.gender = this.formadduser.get('gender').value;
   }
 
 }
